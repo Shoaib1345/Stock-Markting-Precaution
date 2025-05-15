30baa70dda5b1d62e8d91ce2704afa172163798f
Shoaib Ahmed <shoaibahmedprogramming@gmail.com>
2025-05-15 12:26:16 +0500

    Important libaraies for project

---
+++ Main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+from binance.client import Client
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import sqlite3
+import json
+import datetime
+import csv
+
+client = Client()
+
+# ---------- Login Window Class ----------
+class LoginWindow:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Login - Crypto Predictor")
+        self.root.geometry("400x300")
+        self.root.configure(bg="#001f3f")
+
+        self.conn = sqlite3.connect("users.db")
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE,
+                password TEXT
+            )
+        """)
+        self.conn.commit()
